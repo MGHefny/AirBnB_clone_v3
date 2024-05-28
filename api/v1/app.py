8513e27d68ebcf6acb1f2035b8d9158118dc05fa
MGHefny <mgenedhefny@gmail.com>
2024-05-28 03:46:53 +0300

    5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python3
+"""ind"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status')
+def api_stat():
+    """ check """
+    return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def count_stats():
+    '''check'''
+    stats_count = {}
+    for cls in classes:
+        stats_count[cls] = storage.count(classes[cls])
+    return jsonify(stats_count)
+
+
+
+#!/usr/bin/python3
 """host and port"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -9,6 +37,18 @@ from os import getenv
 ap = Flask(__name__)
 
 ap.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def cls_engn(self):
+    """clos engen"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """error 404"""
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == '__main__':
     x = getenv('HBNB_API_HOST', '0.0.0.0')
